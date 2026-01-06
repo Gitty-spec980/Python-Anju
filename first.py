680d5aa0e3b5fb78286c61c42df23bb109e345f1
Gitty-spec980 <allaraji@gmail.com>
2026-01-05 20:22:44 -0500

    initial commit

---
+++ first.py
@@ -0,0 +1,14 @@
+name="Anjali"
+rollNo = 4
+marks=100
+passed=True
+print("My name is",name)
+print(type(passed))
+s="computer"
+print(len(s))
+print(s[3])
+print(s[0:7])
+fname="Anjali" 
+lname="Daggumati"
+print(fname+"  "+lname)
+print(s*3)
