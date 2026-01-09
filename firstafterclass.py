8a86c1fba56e114987d0fcb10c8dca7ef0f67e29
Gitty-spec980 <allaraji@gmail.com>
2026-01-09 15:09:58 -0500

    initial commit

---
+++ firstafterclass.py
@@ -0,0 +1,37 @@
+name="Anjali"
+rollNo = 4
+marks=100
+passed=True
+print("My name is",name)
+print(type(passed))
+s="computer"
+print(len(s))
+print(s[3])
+print(s[0:7])
+fname="Anjali" 
+lname="Daggumati"
+print(fname+"  "+lname)
+print(s*3)
+
+name="7"
+rollNo = 4
+marks=100
+passed=True
+print("I am in Grade",7)
+print(type(passed))
+s="computer"
+print(len(s))
+print(s[3])
+print(s[0:7])
+
+name="12"
+rollNo = 4
+marks=100
+passed=True
+print("I am",12)
+print(type(passed))
+s="computer"
+print(len(s))
+print(s[3])
+print(s[0:7])
+
