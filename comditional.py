5b3674006e33b95737c050bb7a20f14030e98dfc
Gitty-spec980 <allaraji@gmail.com>
2026-01-12 20:28:32 -0500

    initial commit

---
+++ comditional.py
@@ -0,0 +1,9 @@
+num=int(input('Enter your Number'))
+if num>0:
+    print('Number is positive')
+elif num<0:
+    print('Number is negetive')
+elif num==0:
+    print('Number is neutral')
+else:
+    print('Invalid Input,plz try again')
---
+++ list.py
@@ -0,0 +1,25 @@
+colors=['red','orange','purple','blue','pink']
+print(type(colors))
+
+stu=['Anjali',3,94.6,'07/25/2013',['singing','drama','field hockey','dance']]
+print(stu[4])
+print(stu [0:2])
+print (stu[4][1])
+
+stu[1]=4
+print(stu)
+
+
+
+stu.append(676767676767677)
+print(stu)
+
+
+stu.insert(4,141414147041)
+print(stu)
+
+stu.pop()
+print(stu)
+
+stu.remove(94.6)
+print(stu)
