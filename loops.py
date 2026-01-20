b1b945a5aa9fa024bdbdeb8599669f2e611933f2
Gitty-spec980 <allaraji@gmail.com>
2026-01-19 20:37:00 -0500

    initial commit

---
+++ loops.py
@@ -0,0 +1,13 @@
+#Write a Python program for printing the table of 17 (1 to 10), using for loop.
+
+#num=int(input("Enter a number:"))
+#for i in range(1,11):
+    #print(num,"x",i,'=',num*i)
+
+    #Write a Python program for printing the sum of the first ten natural numbers using the while loop.
+num=1
+sum=0
+while(num<=10):
+    sum=sum+num
+    num=num+1
+print("Sum :" , sum)
