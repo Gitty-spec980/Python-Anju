fd3bf0131218793b443d2d22d2ada398a18b5057
Gitty-spec980 <allaraji@gmail.com>
2026-02-02 17:37:31 -0500

    initial commit

--- conditionallistsafterclass.py
+++ conditionallistsafterclass.py
@@ -1,23 +1,9 @@
-def is_natural_number(num_str):
-    """
-    Checks if a string representation is a natural number (positive integer).
-    """
-    try:
-        num = int(num_str)
-        # Check if the number is greater than 0
-        if num > 0:
-            return True
-        else:
-            return False
-    except ValueError:
-        # If conversion to int fails, it's not an integer
-        return False
-
-# Main part of the program
-if __name__ == "__main__":
-    user_input = input("Enter a number: ")
-
-    if is_natural_number(user_input):
-        print(f"'{user_input}' is a natural number.")
-    else:
-        print(f"'{user_input}' is not a natural number.")
+num=int(input('Enter your Number'))
+if num>0:
+    print('Number is Not natural!')
+elif num<0:
+    print('Number is Natural')
+elif num==0:
+    print('Number is natural and is zero:)')
+else:
+    print('Invalid Input,plz try again')
--- loopsafterclass.py
+++ loopsafterclass.py
@@ -1,4 +1,8 @@
-num=int(input("Enter a number:"))
-for i in range(1,11):
-    print(num,"x",i,'=',num**i)
- 
+n = int(input("Enter the number of terms: "))
+
+for i in range(1, n + 1):
+    exponent = i ** i
+    if i < n:
+        print(exponent, end=", ")
+    else:
+        print(exponent)
