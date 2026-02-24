c076fe3a1cca878e0a8f2d23b36701c42a55a1b3
Gitty-spec980 <allaraji@gmail.com>
2026-02-24 15:54:53 -0500

    initial commit

---
+++ operationsafterclass.py
@@ -0,0 +1,14 @@
+name="Anjali, and I am a student at BRMS."
+is_student= True
+
+print("Hello, my name is", name)
+
+s="I love hanging out with my friends"
+print(len(s))
+print(s[5])
+print(s[10:13])
+fname="Anjali"
+lname="and I am a a student at BRMS"
+grade="7th grade"
+
+print("My name is ", fname+" "+lname, " and I am in ", grade)
