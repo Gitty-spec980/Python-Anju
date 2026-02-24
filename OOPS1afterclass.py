ee2643369c182831b67292c3252f93fa2a6188a9
Gitty-spec980 <allaraji@gmail.com>
2026-02-24 15:43:34 -0500

    initial commit

---
+++ OOPS1afterclass.py
@@ -0,0 +1,15 @@
+
+name="Anjali, I am a student in BRMS."
+is_student= True
+
+print("Hello, my name is", name)
+
+s="I love hanging out with my friends"
+print(len(s))
+print(s[5])
+print(s[10:13])
+fname="Anjali"
+lname="I am a student in BRMS"
+grade="7th grade"
+
+print("My name is ", fname+" "+lname, " and I am in curenntly in ", grade)
