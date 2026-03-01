e1ac6bc4dd3893ff833de016a827d9d7fbaf25be
Gitty-spec980 <allaraji@gmail.com>
2026-03-01 16:48:34 -0500

    initial commit

---
+++ operationpart1afterclass.py
@@ -0,0 +1,30 @@
+# f =open("Codingafterclass.txt", "r")
+# print(f.read(10))
+# f.close()
+
+# f =open("Codingafterclass.txt","r")
+# print(f.readlines())
+# f.close
+
+# f =open("Codingafterclass.txt","r")
+# print(f.readlines())
+# print(f.readlines())
+# print(f.readlines())
+# print(f.readlines())
+# f.close
+
+f =open("Codingafterclass.txt","r")
+print(f.readlines())
+print(f.readlines())
+print(f.readlines())
+print(f.readlines())
+print(f.readlines())
+print(f.readlines())
+print(f.readlines())
+print(f.readlines())
+
+
+f.close
+
+
+
