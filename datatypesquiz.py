1ad913d646b418825631615249615233f06381d8
Gitty-spec980 <allaraji@gmail.com>
2026-04-20 20:45:01 -0400

    initial commit

---
+++ datatypesquiz.py
@@ -0,0 +1,37 @@
+# #Assigning Diff variables
+# name = '82'
+# age= 1000
+# is_student=True
+# weight = 100.10
+
+# #printing diff variables and their data type
+# print('Name :', name)
+# print('Data type of name is ', type(name))
+
+# print('Age: ', age)
+# print('Data type of age is ' , type(age))
+
+# #type conversioning
+# a=int(name)
+# print(type(a))
+# w=str(weight)
+# print(type(w))
+
+# #Reversing a string
+# #  input a word
+# text = str(input('Enter a string: '))
+
+# # Reverse string
+# # using a step value as -1 to iterate in revrse
+# revText = text[::-1]
+# text= revText
+# print('Reverse of Give String is: ')
+# print(text)
+
+# input a word
+A='ANJU'
+print(A.lower())
+
+
+
+
