707a4a904aac07a48e30a70d6cc660749c653b10
Gitty-spec980 <allaraji@gmail.com>
2026-04-27 18:22:44 -0400

    initial commit

---
+++ celebrationac.py
@@ -0,0 +1,36 @@
+# text = (input("Enter a string:"))
+
+
+
+# revText = text[::-1]
+# text = revText
+
+# print("Reverse of Given String is:")
+# print(text)
+
+name = "Jay"
+age = 25
+is_student = False
+weight = 95.25
+
+
+
+print("Name:", name)
+print("Data Type of Name is", type(name))
+
+print("Age;", age)
+print("Data type of age is", type(age))
+
+print("is_student :", is_student)
+print("Data type of idol is", type(is_student))
+
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data type of age is", type(age))
+weight=int(weight)
+print(weight)
+print("Data type of age is", type(weight))
