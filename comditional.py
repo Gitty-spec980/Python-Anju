0e736a1b2146a340e2acdd6978470c383d6b2cf3
Gitty-spec980 <allaraji@gmail.com>
2026-05-04 20:42:25 -0400

    initial commit

--- comditional.py
+++ comditional.py
@@ -1,9 +1,37 @@
-num=int(input('Enter your Number'))
-if num>0:
-    print('Number is positive')
-elif num<0:
-    print('Number is negetive')
-elif num==0:
-    print('Number is neutral')
-else:
-    print('Invalid Input,plz try again')
+# num=int(input('Enter your Number'))
+# if num>0:
+#     print('Number is positive')
+# elif num<0:
+#     print('Number is negetive')
+# elif num==0:
+#     print('Number is neutral')
+# else:
+#     print('Invalid Input,plz try again')
+
+# #  Make an Input to check if a person is buying oranges at 100rs  and later selling it at 120 rs. Find that he has profit or loss)
+
+# actual_cost=float(input('Enter  actual price number here'))
+# sell_price=float(input('Enter selling price number here'))
+
+# if actual_cost<sell_price:
+#     print('You have have a profit of ', (sell_price-actual_cost))
+# else:
+#     print('You have have a loss of ', (actual_cost-sell_price))
+
+
+# Temperature advice system
+# Input temperature:
+# < 10 → "Very Cold"
+# 10–25 → "Moderate"
+# 25 → "Hot"
+# Add one more condition: if temperature > 40 → "Extreme Heat Warning"
+
+# temp=int(input('Enter the temp (F)'))
+# if temp<10:
+#     print('VEREY cold...')
+# elif temp>10 and temp<25:
+#     print('Moderate....')
+# elif temp>=25:
+#     print('HOT...')
+# else:
+#     print('EXTREMEe heat warning...')
