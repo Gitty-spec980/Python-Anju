af3ed08b1de576b2bd9ac8a3f4ccb6ee353439a9
Gitty-spec980 <allaraji@gmail.com>
2026-06-15 20:55:20 -0400

    initical commit

---
+++ pythonchallenges.py
@@ -0,0 +1,28 @@
+# a=4
+# b=6
+# c=7
+# d=3
+# e=(a+b) * c / d
+# print('Value of (a+b)*c/d is ',e)
+
+
+# name='Anjali'
+# age = 13
+
+# if name == 'Anjali' or name == "Deepali Mam" and age >=67:
+#     print('hello! Welcome to the 67 Club!')
+# else :
+#     print('OOPS your too young/old byeee')
+
+
+# Write to check a number is divisible by another number.
+
+# numer=int(input('Please enter your numerator'))
+# denom=int(input('Please enter the denominator'))
+# if numer%denom==0:
+#     print(numer,"is completley divisible by ", denom )
+# else:
+#     print('Your numbers are not fully divisible! Please try again.')
+
+
+
