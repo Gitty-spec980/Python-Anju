0f9f5b118fb53ad291c5638c9ca099de2bfd0108
Gitty-spec980 <allaraji@gmail.com>
2026-06-22 18:17:04 -0400

    initial commit

---
+++ pythonoperator2afterclass.py
@@ -0,0 +1,11 @@
+character = "g"
+if character >= "a" and character <= "z":
+    print(character, "is an alphabet.")
+else:
+    print(character, "is not an alphabet.")
+
+character = "4"
+if character >= "a" and character <= "z":
+    print(character, "is an alphabet.")
+else:
+    print(character, "is not an alphabet.")
