46aaf42790de87bd32f4cf9bba635cdb76e5f816
Gitty-spec980 <allaraji@gmail.com>
2026-06-29 12:14:27 -0400

    initial commit

---
+++ nestedconditionalstatementsafterclass.py
@@ -0,0 +1,12 @@
+
+age = int(input("Enter your age: "))
+
+
+if age >= 10:
+    
+    if age <= 20:
+        print("Your age is between 10 and 20 years.")
+    else:
+        print("Your age is not between 10 and 20 years.")
+else:
+    print("Your age is not between 10 and 20 years.")
