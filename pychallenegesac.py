45f1dc1101a7003186cad0c33d609fe7f889cb0e
Gitty-spec980 <allaraji@gmail.com>
2026-06-29 20:38:12 -0400

    intital commit

---
+++ pychallenegesac.py
@@ -0,0 +1,21 @@
+
+
+a = 10
+b = 20
+c = 30
+
+print(f"Before: a={a}, b={b}, c={c}")
+
+a = a + b + c  
+
+
+b = a - (b + c) 
+
+
+c = a - (b + c) 
+
+
+a = a - (b + c)  
+
+print(f"After:  a={a}, b={b}, c={c}")
+
