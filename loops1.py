77aa433333e1f5b36f1e09e818898587a135fa0c
Gitty-spec980 <allaraji@gmail.com>
2026-07-06 20:55:20 -0400

    initial commit

---
+++ loops1.py
@@ -0,0 +1,8 @@
+# Write a program to calculate the sum of whole numbers.
+num=int(input("Enter the number range/how many numbers you want."))
+sum=0
+for i in range (0,num+1):
+    sum= sum+i
+print('The sum is ', sum)
+
+
