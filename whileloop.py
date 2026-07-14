a265f6fca33be29fae092f32b6f21765af53e993
Gitty-spec980 <allaraji@gmail.com>
2026-07-13 20:49:59 -0400

    initial commit

---
+++ whileloop.py
@@ -0,0 +1,32 @@
+# # Write a program to find the sum of natural numbers.
+# s=1
+# sum=0
+# while s <11:
+#     sum= sum+s
+#     s=s+1
+# print("The sum is" , sum)
+
+
+
+# # Write a program to check the infinite loop?
+
+# i=1
+# while i <11 :
+#     print(i)
+
+# Write a program to check if the number entered by the user is an Armstrong number or not?
+
+# num=int(input('Please enter a three digit number'))
+
+# sum=0
+
+# temp=num
+# while temp >0: 
+#     digit = temp % 10
+#     sum += digit **3
+#     temp//=10
+
+# if num== sum:
+#     print( num,' is an Armstrong number!')
+# else:
+#     print(num,"is not an Armstrong number!")
