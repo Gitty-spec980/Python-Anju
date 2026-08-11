eaa9546ec2ea48844afe3785fde4bec5a683f5c7
Gitty-spec980 <allaraji@gmail.com>
2026-08-10 20:52:59 -0400

    initial commit

---
+++ test1.py
@@ -0,0 +1,22 @@
+# Number Guessing Game
+# Build a game where the computer picks a secret number between 1 and 50. You have 5 attempts to guess it. After every wrong guess your program shows a hint telling you how close you are. Remaining lives are shown as hearts after each attempt.
+
+# 💡 Hint: Store your secret number in a variable — for example: secret = 27
+
+
+
+# If you already know the random module, feel free to use it! This test checks your logic (conditions, loops, input/output).
+
+
+# What you need to use
+# ------------------------------------------------------------------------
+# 1.  int(input())       →  to read the player's guess
+# 2.  while loop         →  stops after 5 attempts or when player wins
+# 3.  if/elif/else       →  hint system —
+
+random= int(input("Enter a random number from 1 to 50! "))
+if random >= 1 and
+ 
+    
+
+
